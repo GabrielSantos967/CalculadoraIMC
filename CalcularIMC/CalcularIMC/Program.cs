fe58333fc2707de9d61e9bcaf3b81c6a36a867db
Gabriel Santos <biel2013as@gmail.com>
2025-01-27 20:20:56 -0300

    CalculoIMC

---
+++ CalcularIMC/CalcularIMC/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string peso = "";
+            string altura = "";
+
+            Console.WriteLine("###Calculo IMC###");
+
+            Console.WriteLine("Coloque seu peso");
+
+            peso = Console.ReadLine();
+
+            Console.WriteLine("Coloque sua altura");
+
+            altura = Console.ReadLine();
+
+            double peso2 = double.Parse(peso);
+            double altura2 = double.Parse(altura);
+
+            double resultado = peso2 / (altura2 * altura2);
+
+            Console.WriteLine(resultado.ToString("F"));
+
+            if (resultado <= 18.5)
+            {
+                Console.WriteLine("Abaixo do normal");
+            } else if (resultado >= 18.6 && resultado <= 24.9)
+            {
+                Console.WriteLine("Normal");
+
+            } else if (resultado >= 25 && resultado <= 29.9)
+            {
+                Console.WriteLine("Sobrepeso");
+
+            } else if (resultado >= 30 && resultado <= 34.9)
+            {
+                Console.WriteLine("Obesidade grau 1");
+
+            } else if (resultado >= 35 && resultado <= 39.9)
+            {
+                Console.WriteLine("Obesidade grau 2");
+
+            } else
+            {
+                Console.WriteLine("Obesidade grau 3");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
